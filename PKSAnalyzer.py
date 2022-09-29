7620e32008ad1ea1f02cf6d61244749d2ee84a86
Ximelele <druzbacky.martin@gmail.com>
2022-09-29 11:20:44 +0200

    Pridanie naplania dicts zo suboru. Pridanie upravy pre YAML formatovanie, zdroj v komentari. Dokoncenie YAML vystupu.

--- PKSAnalyzer.py
+++ PKSAnalyzer.py
@@ -1,9 +1,8 @@
 from binascii import hexlify
 from os.path import exists
 
+import yaml
 from scapy.all import *
-
-from Protocols.protocols import inside_protocol
 
 
 # overenie filu a nasledne otvorenie cez scapy
@@ -25,22 +24,25 @@ def get_file():
     return None, err
 
 
+def fill_dict(dict, file):
+    with open(file, 'r') as f:
+        for line in f:
+            f_data = line.split(" ", 1)
+            dict[int(f_data[0], 16)] = f_data[1].rstrip()
+
+
 # ziskanie dat z framu v danom rozsahu
 def frame_data(frame, begin, end):
     return hexlify(frame[begin:(end + 1)])
 
 
-# def get_header_length(frame):
-
-
 def get_frame_length(frame):
-    output = str("")
-    output += f"len_frame_pcap: {len(frame)}\n"
+    output = len(frame)
     if len(frame) + 4 > 64:
-        output += f"len_frame_medium:  {len(frame) + 4}\n"
+        output_m = len(frame) + 4
     else:
-        output += f"len_frame_medium: 64\n"
-    return output
+        output_m = 64
+    return output, output_m
 
 
 def get_header_length(frame):
@@ -51,21 +53,20 @@ def get_header_length(frame):
 
 # vypisanie celeho framu v hexa tvare
 def print_frame_hex(frame):
-    output = str("")
+    output = ''
     for index, frame_v in enumerate(frame):
-        output += (str(hexlify(frame[index:(index + 1)]))[2:4] + ' ')
+        output += str(hexlify(frame[index:(index + 1)]))[2:4]
+
+        if index == 0 or index % 16 != 15 and index != len(frame) - 1:
+            output += ' '
 
         if (index + 1) % 16 == 0:
             output += '\n'
-            continue
 
-        if (index + 1) % 8 == 0:
-            output += ' '
+    if len(frame) % 16 != 0:
+        output += '\n'
 
     return output
-
-
-final_yaml = []
 
 
 def extract_mac(frame):
@@ -98,30 +99,74 @@ def frame_type(frame):
     ieee_type = int(frame_data(frame, 14, 15), 16)
 
     if ether_type >= 1500:
-        return "Ethernet 2\n    "
+        return "Ethernet 2"
     elif ieee_type == 0xAAAA:
-        return "IEEE 802.3 LLC + SNAP\n"
+        return "IEEE 802.3 LLC + SNAP"
     elif ieee_type == 0xFFFF:
-        return "IEEE 802.3 raw\n"
+        return "IEEE 802.3 raw"
     else:
-        return "IEEE 802.3 LLC\n"
+        return "IEEE 802.3 LLC"
 
+
+def inside_protocol(frame, yaml_dic):
+    ether_type = int(frame_data(frame, 12, 13), 16)
+    ieee_type = int(frame_data(frame, 14, 15), 16)
+    eth_dic = {}
+    fill_dict(eth_dic, "Protocols\\eth_typ.txt")
+    ip_head = {}
+    fill_dict(ip_head, "Protocols\\ip_protoly.txt")
+    llc = {}
+    fill_dict(llc, "Protocols\\llc_typ.txt")
+    ports = {}
+
+    if ether_type >= 1500:
+        if ether_type in eth_dic:
+            yaml_dic['ether_type'] = eth_dic.get(ether_type)
+
+        else:
+            print(f"Neznamy port")
+
+        if int(frame_data(frame, 23, 23), 16) == 17:
+            fill_dict(ports, "Protocols\\udp.txt")
+
+        if int(frame_data(frame, 23, 23), 16) == 6:
+            fill_dict(ports, "Protocols\\tcp.txt")
+
+
+# Source: https://stackoverflow.com/questions/8640959/how-can-i-control-what-scalar-form-pyyaml-uses-for-my-data/15423007#15423007
+def str_presenter(dumper, frame):
+    if len(frame.splitlines()) > 1:  # check for multiline string
+        return dumper.represent_scalar('tag:yaml.org,2002:str', frame, style="|")
+
+    return dumper.represent_scalar('tag:yaml.org,2002:str', frame)
+
+
+yaml.add_representer(str, str_presenter)
+
+global_yaml = []
 
 def yaml_out(frame, frame_num):
     output = str("")
     output += f"frame_number: {frame_num}\n"
-    output += get_frame_length(frame)
+    length, media = get_frame_length(frame)
+
     frame_t = frame_type(frame)
     src_mac, dst_mac = get_mac(frame)
     src_ip, dst_ip = get_ip_add(frame)
-    output += f"frame_type: {frame_t}"
     output += f"src_mac: {src_mac}\ndst_mac: {dst_mac}\n"
     output += f"src_ip: {src_ip}\ndst_ip: {dst_ip}\n"
-    output += f"hexa_frame: \n{print_frame_hex(frame)}"
-    print(get_header_length(frame))
-    inside_protocol(frame)
 
-    # print(output)
+    yaml = {}
+    yaml["frame_number"] = frame_num
+    yaml['len_frame_pcap'] = length
+    yaml['len_frame_medium'] = media
+    yaml['frame_type'] = frame_t
+    yaml['src_mac'] = src_mac
+    yaml['dst_mac'] = dst_mac
+
+    inside_protocol(frame, yaml)
+    yaml['hexa_frame'] = print_frame_hex(frame)
+    global_yaml.append(yaml)
 
 
 def ipv4_nodes(frame, nodes):
@@ -134,11 +179,22 @@ def ipv4_nodes(frame, nodes):
 
 
 def print_ipv4_nodes(nodes):
-    for ip in nodes:
-        print(f"node: {extract_ip(ip)}\nnumber_of_sent_packets: {nodes[ip]}")
-
+    send = []
     max_send = max(nodes, key=lambda x: nodes[x])
-    print(f"max_send_packets_by: {extract_ip(max_send)}")
+    max_send = nodes[max_send]
+    max_array = []
+
+    for ip in nodes:
+        if nodes[ip] == max_send:
+            max_array.append(extract_ip(ip))
+        ipv4_senders = {
+            'node': extract_ip(ip),
+            'number_of_sent_packets': nodes[ip]
+        }
+
+        send.append(ipv4_senders)
+
+    return send, max_array
 
 
 # parsovanie na bytes z povodneho filu
@@ -149,12 +205,16 @@ def parse_packet(data):
         frame = bytes(values)
 
         yaml_out(frame, frame_num + 1)
-        break
 
         if int(frame_data(frame, 12, 13), 16) == 2048:
             ipv4_nodes(frame, ip_nodes)
-
-    # print_ipv4_nodes(ip_nodes)
+    nodes, max_sent = print_ipv4_nodes(ip_nodes)
+    packets = {'packets': global_yaml,
+               'ipv4_senders': nodes,
+               'max_send_packets_by':max_sent,
+               }
+    with open('output.yaml', 'w') as f:
+        yaml.dump(packets, f, sort_keys=False, indent=3)
 
 
 # User interface
@@ -168,6 +228,3 @@ while (True):
     print(err)
 
 parse_packet(data)
-
-# with open(r'output.yaml','w') as file:
-#     documents = yaml.dump(final_yaml,file,default_flow_style=False)
