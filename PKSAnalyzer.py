3535851b591f6f94ce8156f3785e39fcdc5a54ca
Ximelele <druzbacky.martin@gmail.com>
2022-10-05 14:08:21 +0200

    Vytvorenie icmp streamov a vypis ich komunikacie. Prerobenie LLC,LLC & SNAP a LLC RAW. Upravene txt subory

--- PKSAnalyzer.py
+++ PKSAnalyzer.py
@@ -5,6 +5,7 @@ import yaml
 from scapy.all import *
 
 
+# class bude pouzite pre tcp porty a tftp
 class TcpStreams:
     dst_ip = []
     src_ip = []
@@ -21,12 +22,31 @@ class TcpStreams:
         self.frames.append(pos + 1)
 
 
+# class bude pouzita pre ICMP a ARP
+class IcmpStream:
+    dst_ip = []
+    src_ip = []
+    dst_mac = []
+    src_mac = []
+    closed = False
+    frames = []
+
+    def __init__(self, f_dst_ip, f_src_ip, f_dst_mac, f_src_mac, pos, f_closed=False):
+        self.dst_ip = f_dst_ip
+        self.src_ip = f_src_ip
+        self.dst_mac = f_dst_mac
+        self.src_mac = f_src_mac
+        self.closed = f_closed
+        self.frames = []
+        self.frames.append(pos + 1)
+
+
 # overenie filu a nasledne otvorenie cez scapy
 def get_file():
     print(f"Cesta k suboru by mala vyzerat nasledovne: D:\\Python projects\pks22\\vzorky_pcap_na_analyzu\\eth-1.pcap ",
           end='\n')
     # file = str(input())
-    file = "D:\\Python projects\pks22\\vzorky_pcap_na_analyzu\\trace-11.pcap"
+    file = "D:\\Python projects\pks22\\vzorky_pcap_na_analyzu\\trace-26.pcap"
     err = None
     if exists(file):
         if ".pcap" in file:
@@ -40,11 +60,13 @@ def get_file():
     return None, err
 
 
-def fill_dict(dict, file):
+def fill_dict(file):
+    dict = {}
     with open(file, 'r') as f:
         for line in f:
             f_data = line.split(" ", 1)
             dict[int(f_data[0], 16)] = f_data[1].rstrip()
+    return dict
 
 
 # ziskanie dat z framu v danom rozsahu
@@ -135,29 +157,27 @@ def ports_out(frame, yaml_dic):
     yaml_dic['dst_port'] = int(get_frame_data(get_header_length(frame), 2, 3), 16)
 
 
-def inside_protocol(frame, yaml_dic):
+gl_menu = ""
+
+
+def inside_protocol(frame, yaml_dic, framen):
     ether_type = int(get_frame_data(frame, 12, 13), 16)
     ieee_type = int(get_frame_data(frame, 14, 15), 16)
-    eth_dic = {}
-    fill_dict(eth_dic, "Protocols\\eth_typ.txt")
-    ip_head = {}
-    fill_dict(ip_head, "Protocols\\ip_protoly.txt")
-    llc = {}
-    fill_dict(llc, "Protocols\\llc_typ.txt")
+    eth_dic = fill_dict("Protocols\\eth_typ.txt")
+    ip_head = fill_dict("Protocols\\ip_protoly.txt")
+    sap = fill_dict("Protocols\\sap.txt")
+    pid = fill_dict("Protocols\\pid.txt")
     ports = {}
     ip_head_num = int(get_frame_data(frame, 23, 23), 16)
     if ether_type >= 1500:
         if ether_type in eth_dic:
             yaml_dic['ether_type'] = eth_dic.get(ether_type)
 
-        else:
-            print(f"Unknown port")
-
         if int(get_frame_data(frame, 23, 23), 16) == 17:
-            fill_dict(ports, "Protocols\\udp.txt")
+            ports = fill_dict("Protocols\\udp.txt")
 
         if int(get_frame_data(frame, 23, 23), 16) == 6:
-            fill_dict(ports, "Protocols\\tcp.txt")
+            ports = fill_dict("Protocols\\tcp.txt")
 
         if ether_type == 2054:
             src_ip, dst_ip = get_ip_add(frame, False)
@@ -175,29 +195,29 @@ def inside_protocol(frame, yaml_dic):
 
             if ip_head_num in ip_head:
                 yaml_dic['protocol'] = ip_head.get(ip_head_num)
-            ports_out(frame, yaml_dic)
-            if int(get_frame_data(get_header_length(frame), 0, 1), 16) in ports:
-                yaml_dic['app_protocol'] = ports.get(int(get_frame_data(get_header_length(frame), 0, 1), 16))
-            else:
-                if int(get_frame_data(get_header_length(frame), 2, 3), 16) in ports:
-                    yaml_dic['app_protocol'] = ports.get(int(get_frame_data(get_header_length(frame), 2, 3), 16))
+            if gl_menu != ("ARP" or "ICMP"):
+                ports_out(frame, yaml_dic)
+                if int(get_frame_data(get_header_length(frame), 0, 1), 16) in ports:
+                    yaml_dic['app_protocol'] = ports.get(int(get_frame_data(get_header_length(frame), 0, 1), 16))
                 else:
-                    yaml_dic['app_protocol'] = str("unknown")
+                    if int(get_frame_data(get_header_length(frame), 2, 3), 16) in ports:
+                        yaml_dic['app_protocol'] = ports.get(int(get_frame_data(get_header_length(frame), 2, 3), 16))
+                    # else:
+                    #     yaml_dic['app_protocol'] = str("unknown")
             if ip_head_num == 1:
-                icmp = {}
-                fill_dict(icmp, "Protocols\\icmp_typ.txt")
+                icmp = fill_dict("Protocols\\icmp_typ.txt")
 
                 if int(get_frame_data(get_header_length(frame), 0, 0), 16) in icmp:
                     yaml_dic["icmp_type"] = icmp.get(int(get_frame_data(get_header_length(frame), 0, 0), 16))
-    elif ieee_type == 0xAAAA:
-        if ether_type in eth_dic:
-            yaml_dic['sap'] = eth_dic.get(ether_type)
+    elif ieee_type == 0xAAAA:  # llc & snap
+        if int(get_frame_data(frame, 20, 21), 16) in pid:
+            yaml_dic['pid'] = pid.get(int(get_frame_data(frame, 20, 21), 16))
         else:
-            yaml_dic['sap'] = str("Unknown")
-    elif ieee_type == 0xFFFF:
-        yaml_dic['sap'] = str("IPX")
-    elif int(get_frame_data(get_header_length(frame), 14, 14), 16) in llc:
-        yaml_dic['sap'] = llc.get(int(get_frame_data(get_header_length(frame), 14, 14), 16))
+            yaml_dic['pid'] = pid.get(int(get_frame_data(frame, 46, 47), 16))
+    elif ieee_type == 0xFFFF:  ##raw
+        return
+    else:
+        yaml_dic['sap'] = sap.get(int(get_frame_data(frame, 14, 14), 16))
 
 
 # Source: https://stackoverflow.com/questions/8640959/how-can-i-control-what-scalar-form-pyyaml-uses-for-my-data/15423007#15423007
@@ -224,7 +244,7 @@ def yaml_out(frame, frame_num, temp_dic):
     yaml_dic['src_mac'] = src_mac
     yaml_dic['dst_mac'] = dst_mac
 
-    inside_protocol(frame, yaml_dic)
+    inside_protocol(frame, yaml_dic, frame_num)
     yaml_dic['hexa_frame'] = print_frame_hex(frame)
     if temp_dic is None:
         global_yaml.append(yaml_dic)
@@ -260,6 +280,7 @@ def print_ipv4_nodes(nodes):
     return send, max_array
 
 
+# pre tcp
 def ip_in_stream(tcp_stream, src_ip, dst_ip, src_port, dst_port):
     for pos in range(len(tcp_stream)):
         if (tcp_stream[pos].src_ip == dst_ip and tcp_stream[pos].dst_ip == src_ip and
@@ -268,6 +289,34 @@ def ip_in_stream(tcp_stream, src_ip, dst_ip, src_port, dst_port):
                  tcp_stream[pos].src_port == src_port and tcp_stream[pos].dst_port == dst_port):
             return pos
     return False
+
+
+def ip_in_stream_icmp(packet_stream, src_ip, dst_ip, src_mac, dst_mac):
+    for pos in range(len(packet_stream)):
+        if packet_stream[pos].closed == True:
+            continue
+        if (packet_stream[pos].src_ip == dst_ip and packet_stream[pos].dst_ip == src_ip and
+            packet_stream[pos].src_mac == dst_mac and packet_stream[pos].dst_mac == src_mac) or \
+                (packet_stream[pos].src_ip == src_ip and packet_stream[pos].dst_ip == dst_ip and
+                 packet_stream[pos].src_mac == src_mac and packet_stream[pos].dst_mac == dst_mac):
+            return pos
+
+    return False
+
+
+def insert_to_icmp(packet_stream, src_ip, dst_ip, src_mac, dst_mac, pos, data):
+    new_stream = ip_in_stream_icmp(packet_stream, src_ip, dst_ip, src_mac, dst_mac)
+
+    if new_stream:
+        packet_stream[new_stream].frames.append(pos + 1)
+        if int(get_frame_data(get_header_length(bytes(data[pos])), 0, 0), 16) == 0x0 or int(
+                get_frame_data(get_header_length(bytes(data[pos])), 0, 0), 16) == 0xB:
+            packet_stream[new_stream].closed = True
+    else:
+        packet_stream.append(IcmpStream(dst_ip, src_ip, dst_mac, src_mac, pos))
+        if int(get_frame_data(get_header_length(bytes(data[pos])), 0, 0), 16) == 0x0 or int(
+                get_frame_data(get_header_length(bytes(data[pos])), 0, 0), 16) == 0xB:
+            packet_stream[new_stream].closed = True
 
 
 def insert_stream(tcp_stream, src_ip, dst_ip, src_port, dst_port, pos):
@@ -348,9 +397,43 @@ def print_tcp_stream(tcp_stream, data):
     return partial_comms_dic, cmplt_comms
 
 
+def print_icnmp_stream(packet_stream, data):
+    number_comm = 1
+    partial_comms = False
+    partial_comms_dic = None
+    cmplt_comms = []
+
+    for pos in packet_stream:
+        complete_comms = dict()
+        begin_com = False
+        end_com = False
+        if (len(pos.frames) > 1):
+            if int(get_frame_data(get_header_length(bytes(data[pos.frames[0] - 1])), 0, 0), 16) == 0x8:
+                begin_com = True
+
+        if pos.closed:
+            end_com = True
+
+        if begin_com and end_com:
+            complete_comms['number_comm'] = number_comm
+            complete_comms['src_comm'] = pos.src_ip
+            complete_comms['dst_comm'] = pos.dst_ip
+            complete_comms['packets'] = print_stream(data, pos.frames)
+            cmplt_comms.append(complete_comms)
+            number_comm += 1
+
+        if not partial_comms:
+            if begin_com or end_com:
+                partial_comms = True
+                partial_comms_dic = print_stream(data, pos.frames)
+
+    return partial_comms_dic, cmplt_comms
+
+
 # parsovanie na bytes z povodneho filu
 def parse_packet(data, menu):
     menu_opt = ["HTTP", "HTTPS", "TELNET", "SSH", "FTP datove", "FTP riadiace"]
+    gl_menu = menu
     if menu == "ALL":
         ip_nodes = {}
 
@@ -372,8 +455,7 @@ def parse_packet(data, menu):
             yaml.dump(packets, f, sort_keys=False, indent=3)
     elif menu in menu_opt:
         tcp_stream = []
-        ports = {}
-        fill_dict(ports, "Protocols\\tcp.txt")
+        ports = fill_dict("Protocols\\tcp.txt")
 
         for frame_num, values in enumerate(data):
             frame = bytes(values)
@@ -397,6 +479,23 @@ def parse_packet(data, menu):
                    }
         with open('output.yaml', 'w') as f:
             yaml.dump(packets, f, sort_keys=False, indent=3)
+    elif menu == "ICMP":
+        icmp_stream = []
+        for frame_num, values in enumerate(data):
+            frame = bytes(values)
+            src_ip, dst_ip = get_ip_add(frame)
+            src_mac, dst_mac = get_mac(frame)
+            if int(get_frame_data(frame, 23, 23), 16) == 1:
+                insert_to_icmp(icmp_stream, src_ip, dst_ip, src_mac, dst_mac, frame_num, data)
+        partial_comms, complete_comms = print_icnmp_stream(icmp_stream, data)
+
+        packets = {'name': "PKS2022/23",
+                   'pcap_name': menu + '.pcap',
+                   'complete_comms': complete_comms,
+                   'partial_comms': partial_comms,
+                   }
+        with open('output.yaml', 'w') as f:
+            yaml.dump(packets, f, sort_keys=False, indent=3)
 
 
 # User interface
@@ -409,4 +508,4 @@ while (True):
         break
     print(err)
 
-parse_packet(data, "HTTP")
+parse_packet(data, "ALL")
